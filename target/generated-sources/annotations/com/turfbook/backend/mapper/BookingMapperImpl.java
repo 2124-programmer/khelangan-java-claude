ab2eecb10c01cf6b3d02f27a5fdf4c067f12b803
2124-programmer <2124.programmer@gmail.com>
2026-06-01 23:24:59 +0530

    gitignore updated

--- target/generated-sources/annotations/com/turfbook/backend/mapper/BookingMapperImpl.java
+++ target/generated-sources/annotations/com/turfbook/backend/mapper/BookingMapperImpl.java
@@ -11,8 +11,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-06-01T23:04:16+0530",
-    comments = "version: 1.6.3, compiler: Eclipse JDT (IDE) 3.46.0.v20260407-0427, environment: Java 21.0.10 (Eclipse Adoptium)"
+    date = "2026-06-01T23:09:21+0530",
+    comments = "version: 1.6.3, compiler: javac, environment: Java 21.0.9 (Oracle Corporation)"
 )
 @Component
 public class BookingMapperImpl implements BookingMapper {
